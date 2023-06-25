7e79379d850aa0046d91d16156fc24fdad6e4047
arimateia-souza <arimateia.souza.123@ufrn.edu.br>
2023-06-24 21:42:14 -0300

    initial commit

---
+++ src/main/java/eajufrn/fruitsystem2/FruitSystem2Application.java
@@ -0,0 +1,13 @@
+package eajufrn.fruitsystem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitSystem2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitSystem2Application.class, args);
+    }
+
+}
