0ad4f0494805b234d7aa454f5ce93fbf1ec6c44e
arimateia-souza <arimateia.souza.123@ufrn.edu.br>
2023-07-08 00:05:00 -0300

    DTO endereço

--- src/main/java/eajufrn/fruitsystem2/domain/Endereco.java
+++ src/main/java/eajufrn/fruitsystem2/domain/Endereco.java
@@ -1,12 +1,20 @@
 package eajufrn.fruitsystem2.domain;
 
+import eajufrn.fruitsystem2.controllers.FrutaController;
+import eajufrn.fruitsystem2.controllers.UsuarioController;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.RepresentationModel;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
@@ -18,11 +26,39 @@ public class Endereco extends AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty(message = "O campo 'Cidade' não pode ser vazio")
     private String cidade;
-    @NotEmpty(message = "O campo 'Rua' não pode ser vazio")
     private String rua;
 
     @OneToOne
     private Usuario usuario;
+
+    @Data
+    public static class DtoRequest{//aceita apenas nome, descrição, preço e categoria por exemplo:
+        @NotEmpty(message = "O campo 'Cidade' não pode ser vazio")
+        String cidade;
+        @NotEmpty(message = "O campo 'Rua' não pode ser vazio")
+        String rua;
+
+
+        public static Endereco convertToEntity(Endereco.DtoRequest dto, ModelMapper mapper){
+            return mapper.map(dto, Endereco.class);
+        }
+
+    }
+
+
+    @Data
+    public static class DtoResponse extends RepresentationModel<Endereco.DtoResponse> {
+        String cidade;
+        Double nome;
+
+        public static Endereco.DtoResponse convertToDto(Endereco f, ModelMapper mapper){
+            return mapper.map(f, Endereco.DtoResponse.class); //retorna apenas os atributos do response
+
+
+        }
+
+
+    }
+
 }
